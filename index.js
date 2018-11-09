a9d758ee031271b0731287b5b71341e174fe4a9e
Ulisses Lopes <ulissesdvl@yahoo.com.br>
2018-11-09 05:31:23 -0300

    Using intersectional observer to change nav and header style

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let nav = document.getElementById('main-navigation');
+let portfolio = document.querySelector('#portfolio');
+let home = document.querySelector('#home');
+let contato = document.querySelector('#contato');
+let sobre = document.querySelector('#sobre');
+let header = document.querySelector('header');
 
+//usando no form
 function changeFocus(event) {
 	
 	let el = event.target;
@@ -17,25 +23,45 @@ function changeFocus(event) {
 	
 }
 
-//evento PLACEHOLDER
-// capturar pelo scroll usando a intersection observer api
-nav.addEventListener('click', (event) => {
-	
-	let active = document.getElementsByClassName('active');
-	
-	let el = event.target;
-	
-	if(el.matches('.active')) return;
-	
-	if(!el.matches('.nav-item')) return;
+
+function changeActiveClass(id) {
 	
 	document.querySelector('.active').classList.remove('active');
-	
+	el = document.querySelector(`#nav-${id}`);
 	el.classList.add('active');
-});
+	
+}
 
-let portfolio = document.querySelector('#portfolio');
+var options = {
+	root: null,
+	rootMargin: '0px',
+	threshold: 0.8
+}
 
+var observer = new IntersectionObserver(callback, options);
+
+function callback(entries, observer) {
+	entries.forEach(entry => {
+		if(entry.isIntersecting) {
+			changeActiveClass(entry.target.id);
+		}
+		
+		if(entry.target.id == 'home') {
+			if(entry.isIntersecting)
+				header.classList.add('header-top');
+			else
+				header.classList.remove('header-top');
+		}
+  });
+}
+
+observer.observe(portfolio);
+observer.observe(contato);
+observer.observe(home);
+observer.observe(sobre);
+
+
+//alterar para o clique em qualquer área da foto
 portfolio.addEventListener('click', (event) => {
 	let el = event.target;
 	if(!el.matches('.btn-open') && !el.matches('.btn-open span')) return;
